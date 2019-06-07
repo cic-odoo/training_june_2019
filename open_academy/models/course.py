1c5597af3aa907a589e7b0268c9931a1e7b46019
cic-odoo <cic@odoo.com>
2019-06-06 17:42:34 -0700

    [ADD]DAY 4

---
+++ open_academy/models/course.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'open_academy.course'
+    _description = 'Course info'
+    
+    name = fields.Char(string='Title', required=True)
+    level = fields.Selection([
+        ('easy', 'Easy'),
+        ('normal', 'Normal'),
+        ('hard', 'Hard'),
+    ], string='Difficulty')
+    
+    description = fields.Text(string="Description")
